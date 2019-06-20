f1de8be810655d8c9d702bee2ebf6d355e6a85bf
artem.lupyr <artem.lupyr@ardas.biz>
2019-06-20 17:53:33 +0300

    Send mail only To from settings

--- InsertEmailTemplates.sql
+++ InsertEmailTemplates.sql
@@ -30,7 +30,7 @@ INSERT INTO [dbo].[MessageTemplates]
            , GETUTCDATE()
            ,'offline'
            ,'VSP {udid} is offline'
-           ,'<p>Your VSP last time was online at {lastheartbeat}</p><p></p><p>The VSP is located at this address:</p><p>{address}<p>{town}, {postcode}<p>{country}<p>{notes}.<p></p>
+           ,'<p>Your VSP last time was online at {lastheartbeat}</p><p></p><p>The VSP is located at this address:</p><p>Address: {address}<p>Town: {town}, Post Code: {postcode}<p>Country: {country}<p>Notes: {notes}<p></p>
 <p>This may be due to a power outage, the VSP being disconnected from power or the internet, or an outage of your ISP.
 Feedback can be sent to support form on mailto:<a href = "mailto: {mailto}">{mailto}</a></p>'
            ,'A1E4925F-D2E3-450C-8A99-27EC5F6899B2'),
@@ -40,7 +40,7 @@ Feedback can be sent to support form on mailto:<a href = "mailto: {mailto}">{mai
            ,'online'
            ,'VSP {udid} is online'
            ,'<p>VSP has come back online at {lastheartbeat}</p><p></p><p>Before that we received a heartbeat from your router last time at {previousheartbeat}</p>
-		   <p>The VSP is located at this address:</p><p>{address}<p>{town}, {postcode}<p>{country}<p>{notes}.<p></p>'
+		   <p>The VSP is located at this address:</p><p>Address: {address}<p>Town: {town}, Post Code: {postcode}<p>Country: {country}<p>Notes: {notes}<p></p>'
            ,'A1E4925F-D2E3-450C-8A99-27EC5F6899B2'),
 		   ('0D106E34-B607-452C-867A-B7FC31398722'
            ,0
@@ -48,7 +48,7 @@ Feedback can be sent to support form on mailto:<a href = "mailto: {mailto}">{mai
            ,'outoflocation'
            ,'VSP {udid} is out of location'
            ,'<p>VSP was out of location at {lastheartbeat}</p><p></p>
-		   <p>The VSP is located at this address:</p><p>{address}<p>{town}, {postcode}<p>{country}<p>{notes}.<p></p>
+		   <p>The VSP is located at this address:</p><p>Address: {address}<p>Town: {town}, Post Code: {postcode}<p>Country: {country}<p>Notes: {notes}<p></p>
 		   <p>Feedback can be sent to support form on mailto:<a href = "mailto: {mailto}">{mailto}</a></p>'
            ,'A1E4925F-D2E3-450C-8A99-27EC5F6899B2')
 GO
