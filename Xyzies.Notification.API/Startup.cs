e941bf7d89598c2e6fcc75d6e609496b72a9dbf8
artem.lupyr <artem.lupyr@ardas.biz>
2019-07-15 13:39:31 +0300

    build

--- Xyzies.Notification.API/Startup.cs
+++ Xyzies.Notification.API/Startup.cs
@@ -111,7 +111,7 @@ namespace Xyzies.Notification.API
         }
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
