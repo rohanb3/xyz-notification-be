79835710938984fbf8dd0aa43a3cb7ff915dfc8c
artem.lupyr <artem.lupyr@ardas.biz>
2019-07-15 12:16:22 +0300

    release

--- Xyzies.Notification.API/Startup.cs
+++ Xyzies.Notification.API/Startup.cs
@@ -40,7 +40,7 @@ namespace Xyzies.Notification.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container..
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
