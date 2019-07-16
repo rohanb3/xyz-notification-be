2bc6efd87cbf075f21dabb85d411c14f46d16be0
Arty <kapitaneo@gmail.com>
2019-07-16 15:32:45 +0300

    update logstash build

--- Xyzies.Notification.API/Startup.cs
+++ Xyzies.Notification.API/Startup.cs
@@ -114,7 +114,7 @@ namespace Xyzies.Notification.API
         }
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
